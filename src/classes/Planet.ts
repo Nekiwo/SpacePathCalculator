2edff965d3e8bcc9003d117e9c8ee52251e7f1c4
Nekiwo <geo.panchev@gmail.com>
2021-01-29 15:26:19 -0600

    added a planet class for planets

---
+++ src/classes/Planet.ts
@@ -0,0 +1,17 @@
+// Planet class for each planet
+export class Planet {
+    name: string;
+    radius: number;
+    euler: {
+        x: number;
+        y: number;
+        z: number;
+    }
+    model: string;
+    RadiusFromSun: number;
+    SunDeg: number;
+    constructor(args?: any) {
+        // initiate each planet
+
+    }
+}
