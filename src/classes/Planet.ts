42999c116baf9b9da12b4c44e06c8c82f505b6bb
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-01-30 16:17:57 -0600

    added deleted functions in the class

--- src/classes/Planet.ts
+++ src/classes/Planet.ts
@@ -1,5 +1,3 @@
-import {DrawPlanet} from "../functions/DrawPlanet"
-
 // Planet class for each planet
 export class Planet {
     name: string;
@@ -14,7 +12,7 @@ export class Planet {
     SunDeg: number;
     constructor(args?: any) {
         // initiate each planet
-        DrawPlanet({
+        this.DrawPlanet({
             name: this.name,
             radius: this.radius,
             euler: this.euler,
@@ -22,5 +20,16 @@ export class Planet {
             RadiusFromSun: this.RadiusFromSun,
             SunDeg: this.SunDeg
         });
+    }
+    DrawPlanet = (args?: any) => {
+        // Draw each planet and path on a canvas element for purely testing/showcase purposes
+        // If you want to change the way planets are rendered, for example impliment it all in WebGL, you can do it here
+        let canvas:any = document.getElementById("c");
+        let planet:CanvasRenderingContext2D = canvas.getContext("2d");
+        
+    }
+    ChangePlanetPos = (args: any) => {
+        // Change planet position through degrees on its circumference around the sun
+
     }
 }
