5d64fccb8359df2d88682c13c9d8665d9bf258fe
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-01-30 16:17:20 -0600

    added some more planet info

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
 
 // importing classes and etc.
 import {Planet} from "./classes/Planet";
+import {DrawPath} from "./functions/DrawPath";
 
 // all planets in one array
 var planets:Planet[] = [
@@ -10,108 +11,108 @@ var planets:Planet[] = [
         name: "Mercury",
         radius: 2439,
         euler: {
-            x: 1,
-            y: 1,
-            z: 1
+            x: 0,
+            y: 0,
+            z: 0
         },
         model: "",
-        RadiusFromSun: 1,
+        RadiusFromSun: 57,
         SunDeg: 0
     }),
     new Planet({
         name: "Venus",
         radius: 6502,
         euler: {
-            x: 1,
-            y: 1,
-            z: 1
+            x: 0,
+            y: 0,
+            z: 0
         },
         model: "",
-        RadiusFromSun: 1,
+        RadiusFromSun: 108,
         SunDeg: 0
     }),
     new Planet({
         name: "Earth",
         radius: 6378,
         euler: {
-            x: 1,
-            y: 1,
-            z: 1
+            x: 0,
+            y: 0,
+            z: 0
         },
         model: "",
-        RadiusFromSun: 1,
+        RadiusFromSun: 149,
         SunDeg: 0
     }),
     new Planet({
         name: "Mars",
         radius: 3396,
         euler: {
-            x: 1,
-            y: 1,
-            z: 1
+            x: 0,
+            y: 0,
+            z: 0
         },
         model: "",
-        RadiusFromSun: 1,
+        RadiusFromSun: 228,
         SunDeg: 0
     }),
     new Planet({
         name: "Jupiter",
         radius: 71492,
         euler: {
-            x: 1,
-            y: 1,
-            z: 1
+            x: 0,
+            y: 0,
+            z: 0
         },
         model: "",
-        RadiusFromSun: 1,
+        RadiusFromSun: 780,
         SunDeg: 0
     }),
     new Planet({
         name: "Saturn",
         radius: 60268,
         euler: {
-            x: 1,
-            y: 1,
-            z: 1
+            x: 0,
+            y: 0,
+            z: 0
         },
         model: "",
-        RadiusFromSun: 1,
+        RadiusFromSun: 1437,
         SunDeg: 0
     }),
     new Planet({
         name: "Uranus",
         radius: 25559,
         euler: {
-            x: 1,
-            y: 1,
-            z: 1
+            x: 0,
+            y: 0,
+            z: 0
         },
         model: "",
-        RadiusFromSun: 1,
+        RadiusFromSun: 2871,
         SunDeg: 0
     }),
     new Planet({
         name: "Neptune",
         radius: 24764,
         euler: {
-            x: 1,
-            y: 1,
-            z: 1
+            x: 0,
+            y: 0,
+            z: 0
         },
         model: "",
-        RadiusFromSun: 1,
+        RadiusFromSun: 4530,
         SunDeg: 0
     }),
     new Planet({
         name: "Pluto",
         radius: 1185,
         euler: {
-            x: 1,
-            y: 1,
-            z: 1
+            x: 0,
+            y: 0,
+            z: 0
         },
         model: "",
-        RadiusFromSun: 1,
+        RadiusFromSun: 5900,
         SunDeg: 0
     })
 ];
