cdd0b561d142217d3cd1dbb926df680843cf0fa6
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-01-31 21:27:32 -0600

    continueing work on visual representaion of algo

--- src/classes/Planet.ts
+++ src/classes/Planet.ts
@@ -3,11 +3,6 @@ export class Planet {
     name: string;
     radius: number;
     mass: number;
-    euler: {
-        x: number;
-        y: number;
-        z: number;
-    }
     model: string;
     RadiusFromSun: number;
     SunDeg: number;
@@ -15,20 +10,15 @@ export class Planet {
         this.name = args.name;
         this.radius = args.radius;
         this.mass = args.mass;
-        this.euler = args.euler;
         this.model = args.model;
         this.RadiusFromSun = args.RadiusFromSun;
         this.SunDeg = args.SunDeg;
         // initiate each planet
 
         // draw first frame
-        this.DrawPlanet({
-            radius: this.radius,
-            euler: this.euler,
-            SunDeg: this.SunDeg
-        });
+        this.DrawPlanet(this.SunDeg);
     }
-    DrawPlanet = (args: any) => {
+    DrawPlanet = (SunDeg: number) => {
         // Draw each planet and path on a canvas element for purely testing/showcase purposes
         // If you want to change the way planets are rendered, for example impliment it all in WebGL, you can do it here
         let canvas: HTMLCanvasElement = <HTMLCanvasElement> document.getElementById("c");
@@ -36,17 +26,15 @@ export class Planet {
         let planet:CanvasRenderingContext2D = canvas.getContext("2d");
         let circum:CanvasRenderingContext2D = canvas.getContext("2d");
         
+
+        var ConvertedRadius: number = this.RadiusFromSun / 5;
         circum.beginPath();
-        circum.arc(1500, 1500, this.RadiusFromSun / 5, 0, 2 * Math.PI);
+        circum.arc(1500, 1500, ConvertedRadius, 0, 2 * Math.PI);
         circum.stroke();
 
         planet.fillStyle = "#ff0000";
         planet.beginPath();
-        planet.arc(1500, this.RadiusFromSun / 5 + 1500, 5, 0, 2 * Math.PI);
+        planet.arc(1500 + ConvertedRadius * Math.cos((SunDeg - 90) * (Math.PI/180)), 1500 + ConvertedRadius * Math.sin((SunDeg - 90) * (Math.PI/180)), 5, 0, 2 * Math.PI);
         planet.fill();
-    }
-    ChangePlanetPos = (args: any) => {
-        // Change planet position through degrees on its circumference around the sun
-        
     }
 }
