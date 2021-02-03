e4e07e0493aba0831454c5b7333508dfaa3d4f87
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-02-03 15:35:01 -0600

    started working on the algo

--- src/functions/DrawPath.ts
+++ src/functions/DrawPath.ts
@@ -1,4 +1,12 @@
+import { planets } from "..";
+
 // Draw spaceship travel path
 export var DrawPath = (args: any) => {
-    
+    let canvas: HTMLCanvasElement = <HTMLCanvasElement> document.getElementById("c");
+    let path:CanvasRenderingContext2D = canvas.getContext("2d");
+
+    let StartingPoint:number[] = [planets[3].x, planets[3].y]
+    path.strokeStyle = "#000000";
+    path.moveTo(StartingPoint[0], StartingPoint[1]);
+    path.beginPath();
 }
