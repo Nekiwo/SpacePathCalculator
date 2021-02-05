1e782c757366e2f89c841a9e7e926413611901c7
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-02-04 22:09:11 -0600

    started working on the calculation part

--- src/functions/DrawPath.ts
+++ src/functions/DrawPath.ts
@@ -1,11 +1,26 @@
-import { planets } from "..";
+import {planets} from "..";
 
 // Draw spaceship travel path
 export var DrawPath = (args: any) => {
     let canvas: HTMLCanvasElement = <HTMLCanvasElement> document.getElementById("c");
     let path:CanvasRenderingContext2D = canvas.getContext("2d");
 
-    let StartingPoint:number[] = [planets[3].x, planets[3].y]
+    // starting point
+    let StartingPoint:number[] = [planets[3].x, planets[3].y];
+    // boolean array of planets to wrap
+    let WrapPlanets:boolean[] = [];
+    // 2D array of points for the path to wrap around
+    let WrapPoints:number[][] = [];
+
+    // Change planet positions depending on user input
+    if (args.type === 0) {
+        
+    } else {
+
+    }
+
+
+    // drawing the line
     path.strokeStyle = "#000000";
     path.moveTo(StartingPoint[0], StartingPoint[1]);
     path.beginPath();
