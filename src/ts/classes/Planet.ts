ce47b11553051de35589bfbe30696e920f0647e7
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-03-01 16:00:30 -0600

    moved some files

---
+++ src/ts/classes/Planet.ts
@@ -0,0 +1,44 @@
+// Planet class for each planet
+export class Planet {
+    name: string;
+    radius: number;
+    mass: number;
+    model: string;
+    RadiusFromSun: number;
+    SunDeg: number;
+    x: number;
+    y: number;
+    constructor(args?: any) {
+        this.name = args.name;
+        this.radius = args.radius;
+        this.mass = args.mass;
+        this.model = args.model;
+        this.RadiusFromSun = args.RadiusFromSun;
+        this.SunDeg = args.SunDeg;
+        this.x = 1500 + (this.RadiusFromSun / 5) * Math.cos((this.SunDeg - 90) * (Math.PI/180));
+        this.y = 1500 + (this.RadiusFromSun / 5) * Math.sin((this.SunDeg - 90) * (Math.PI/180));
+        // initiate each planet
+
+        // draw first frame
+        this.DrawPlanet(this.SunDeg);
+    }
+    DrawPlanet: Function = (SunDeg: number) => {
+        // Draw each planet and path on a canvas element for purely testing/showcase purposes
+        // If you want to change the way planets are rendered, for example impliment it all in WebGL, you can do it here
+        let canvas: HTMLCanvasElement = <HTMLCanvasElement> document.getElementById("c");
+        
+        let planet:CanvasRenderingContext2D = canvas.getContext("2d");
+        let circum:CanvasRenderingContext2D = canvas.getContext("2d");
+        
+
+        var ConvertedRadius: number = this.RadiusFromSun / 5;
+        circum.beginPath();
+        circum.arc(1500, 1500, ConvertedRadius, 0, 2 * Math.PI);
+        circum.stroke();
+
+        planet.fillStyle = "#ff0000";
+        planet.beginPath();
+        planet.arc(1500 + ConvertedRadius * Math.cos((SunDeg - 90) * (Math.PI/180)), 1500 + ConvertedRadius * Math.sin((SunDeg - 90) * (Math.PI/180)), 5, 0, 2 * Math.PI);
+        planet.fill();
+    }
+}
---
+++ src/ts/index.ts
@@ -0,0 +1,100 @@
+// NOTE: All functions with more than 1 argument are using one JSON tree as one argument so its easier to understand and see what items are used
+
+
+// importing classes and etc.
+import {Planet} from "./classes/Planet";
+import {ConfigPrompt} from "./functions/ConfigPrompt";
+import {DrawPath} from "./functions/DrawPath";
+
+// all planets in one array
+
+// NOTE: radius of planets is written in km
+// ANOTHER NOTE: radius of planet's sun orbit is written in millions of km
+// ANOTHER ANOTHER NOTE: mass of planets is written in 10^24kg
+export var planets:Planet[] = [
+    new Planet({
+        name: "Sun",
+        radius: 696340000,
+        mass: 1988500,
+        model: "",
+        RadiusFromSun: 0,
+        SunDeg: 0
+    }),
+    new Planet({
+        name: "Mercury",
+        radius: 2439,
+        mass: 0.33,
+        model: "",
+        RadiusFromSun: 57,
+        SunDeg: 0
+    }),
+    new Planet({
+        name: "Venus",
+        radius: 6502,
+        mass: 4.87,
+        model: "",
+        RadiusFromSun: 108,
+        SunDeg: 0
+    }),
+    new Planet({
+        name: "Earth",
+        radius: 6378,
+        mass: 5.97,
+        model: "",
+        RadiusFromSun: 149,
+        SunDeg: 0
+    }),
+    new Planet({
+        name: "Mars",
+        radius: 3396,
+        mass: 0.642,
+        model: "",
+        RadiusFromSun: 228,
+        SunDeg: 0
+    }),
+    new Planet({
+        name: "Jupiter",
+        radius: 71492,
+        mass: 1898,
+        model: "",
+        RadiusFromSun: 780,
+        SunDeg: 0
+    }),
+    new Planet({
+        name: "Saturn",
+        radius: 60268,
+        mass: 568,
+        model: "",
+        RadiusFromSun: 1437,
+        SunDeg: 0
+    }),
+    new Planet({
+        name: "Uranus",
+        radius: 25559,
+        mass: 86.8,
+        model: "",
+        RadiusFromSun: 2871,
+        SunDeg: 0
+    }),
+    new Planet({
+        name: "Neptune",
+        radius: 24764,
+        mass: 102,
+        model: "",
+        RadiusFromSun: 4530,
+        SunDeg: 0
+    }),
+    new Planet({
+        name: "Pluto",
+        radius: 1185,
+        mass: 0.0146,
+        model: "",
+        RadiusFromSun: 5900,
+        SunDeg: 0
+    })
+];
+
+ConfigPrompt().then(config => {
+    // Do something when user got prompted about the config
+    DrawPath(config);
+})
