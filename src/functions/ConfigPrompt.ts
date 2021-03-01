54be1b03fed91df1f098c74a64ffc00da91551b5
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-03-01 15:59:14 -0600

    moved TS files into one folder

--- src/functions/ConfigPrompt.ts
+++
@@ -1,7 +0,0 @@
-// Async function to prompt the user the config options, you may change this into whatever
-export var ConfigPrompt = async (): Promise<void> => {
-    const result: any = {};
-    result.planet = prompt("Choose planet to go to from 1 to 9");
-    result.type = prompt("type 0 if you want to go to the planet now, or type 1 if you can wait until the planets align perfectly");
-    return Promise.resolve(result);
-}
