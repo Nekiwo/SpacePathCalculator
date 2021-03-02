7004f4fe48e10de49f0acf9baaab3e8bca332920
Nekiwo <geo.panchev@gmail.com>
2021-03-01 21:46:12 -0600

    some syntax edits

--- src/ts/functions/ConfigPrompt.ts
+++ src/ts/functions/ConfigPrompt.ts
@@ -1,7 +1,7 @@
 // Async function to prompt the user the config options, you may change this into whatever
-export var ConfigPrompt: Function = async (): Promise<any> => {
+export var ConfigPrompt = new Promise((resolve) => {
     const result: any = {};
     result.planet = prompt("Choose planet to go to from 1 to 9");
     result.type = prompt("type 0 if you want to go to the planet now, or type 1 if you can wait until the planets align perfectly");
-    Promise.resolve(result);
-}
+    resolve(result);
+});
