af7abd959ad0afc0c620ebaf0ae26e4dc50d4bed
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-03-02 21:40:22 -0600

    added more info to promtp

--- src/ts/functions/ConfigPrompt.ts
+++ src/ts/functions/ConfigPrompt.ts
@@ -1,7 +1,7 @@
 // Async function to prompt the user the config options, you may change this into whatever
 export var ConfigPrompt = new Promise((resolve) => {
     const result: any = {};
-    result.planet = prompt("Choose planet to go to from 1 to 9");
+    result.planet = prompt("Choose planet to go to from 1 to 9(except 3, earth)");
     result.type = prompt("type 0 if you want to go to the planet now, or type 1 if you can wait until the planets align perfectly");
     resolve(result);
 });
