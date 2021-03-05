5db11d7c4358b6a2f22a915c5969806e8c6f380f
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-03-04 21:40:18 -0600

    adapted planets to new args

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -18,80 +18,110 @@ export var planets:Planet[] = [
         radius: 696340000,
         mass: 1988500,
         model: "",
-        RadiusFromSun: 0,
-        SunDeg: 0
+        SunDeg: 0,
+        XRadius: 0,
+        ZRadius: 0,
+        XTilt: 0,
+        ZTilt: 0
     }),
     new Planet({
         name: "Mercury",
         radius: 2439,
         mass: 0.33,
         model: "",
-        RadiusFromSun: 57,
-        SunDeg: 0
+        SunDeg: 0,
+        XRadius: 57,
+        ZRadius: 57,
+        XTilt: 0,
+        ZTilt: 0
     }),
     new Planet({
         name: "Venus",
         radius: 6502,
         mass: 4.87,
         model: "",
-        RadiusFromSun: 108,
-        SunDeg: 0
+        SunDeg: 0,
+        XRadius: 108,
+        ZRadius: 108,
+        XTilt: 0,
+        ZTilt: 0
     }),
     new Planet({
         name: "Earth",
         radius: 6378,
         mass: 5.97,
         model: "",
-        RadiusFromSun: 149,
-        SunDeg: 0
+        SunDeg: 0,
+        XRadius: 149,
+        ZRadius: 149,
+        XTilt: 0,
+        ZTilt: 0
     }),
     new Planet({
         name: "Mars",
         radius: 3396,
         mass: 0.642,
         model: "",
-        RadiusFromSun: 228,
-        SunDeg: 0
+        SunDeg: 0,
+        XRadius: 228,
+        ZRadius: 228,
+        XTilt: 0,
+        ZTilt: 0
     }),
     new Planet({
         name: "Jupiter",
         radius: 71492,
         mass: 1898,
         model: "",
-        RadiusFromSun: 780,
-        SunDeg: 0
+        SunDeg: 0,
+        XRadius: 780,
+        ZRadius: 780,
+        XTilt: 0,
+        ZTilt: 0
     }),
     new Planet({
         name: "Saturn",
         radius: 60268,
         mass: 568,
         model: "",
-        RadiusFromSun: 1437,
-        SunDeg: 0
+        SunDeg: 0,
+        XRadius: 1437,
+        ZRadius: 1437,
+        XTilt: 0,
+        ZTilt: 0
     }),
     new Planet({
         name: "Uranus",
         radius: 25559,
         mass: 86.8,
         model: "",
-        RadiusFromSun: 2871,
-        SunDeg: 0
+        SunDeg: 0,
+        XRadius: 2871,
+        ZRadius: 2871,
+        XTilt: 0,
+        ZTilt: 0
     }),
     new Planet({
         name: "Neptune",
         radius: 24764,
         mass: 102,
         model: "",
-        RadiusFromSun: 4530,
-        SunDeg: 0
+        SunDeg: 0,
+        XRadius: 4530,
+        ZRadius: 4530,
+        XTilt: 0,
+        ZTilt: 0
     }),
     new Planet({
         name: "Pluto",
         radius: 1185,
         mass: 0.0146,
         model: "",
-        RadiusFromSun: 5900,
-        SunDeg: 0
+        SunDeg: 0,
+        XRadius: 5900,
+        ZRadius: 5900,
+        XTilt: 0,
+        ZTilt: 0
     })
 ];
 
