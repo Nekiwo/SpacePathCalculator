e3ddfcd54a25432e5bb8234d0aa4dd94c0d3b852
Nekiwo <geo.panchev@gmail.com>
2021-03-06 17:55:41 -0600

    three ts scene

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -6,6 +6,15 @@ import {Planet} from "./classes/Planet";
 import {ConfigPrompt} from "./functions/ConfigPrompt";
 import {DrawPath} from "./functions/DrawPath";
 import {CalcTime} from "./functions/CalcTime";
+import * as THREE from "three";
+
+// Making a three js scene
+const scene:THREE.Scene = new THREE.Scene();
+const camera:THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer:THREE.WebGLRenderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById("webgl").appendChild(renderer.domElement);
 
 // all planets in one array
 
