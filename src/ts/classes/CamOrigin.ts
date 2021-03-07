df044bfcf4ac2d4c5d004b704881907c1651d41e
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-03-06 22:12:49 -0600

    new class for cam position

---
+++ src/ts/classes/CamOrigin.ts
@@ -0,0 +1,19 @@
+// Camera origin class
+export class CamOrigin {
+    x: number = 0;
+    y: number = 0;
+    z: number = 0;
+    euler: {
+        x: number;
+        y: number;
+        z: number;
+    }
+    constructor(args?: any) {
+        this.euler.x = 0;
+        this.euler.y = 0;
+        this.euler.z = 0;
+    }
+    pos: Function = (vector: string, value: number) => {
+        this[vector] = value;
+    }
+}
