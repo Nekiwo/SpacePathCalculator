91a9a13e1650c9da555295a6aa4ecb8faa3e0242
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-03-06 22:12:32 -0600

    did some more work on 3d scene

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -3,18 +3,13 @@
 
 // importing classes and etc.
 import {Planet} from "./classes/Planet";
+import {CamOrigin} from "./classes/CamOrigin";
+
 import {ConfigPrompt} from "./functions/ConfigPrompt";
 import {DrawPath} from "./functions/DrawPath";
 import {CalcTime} from "./functions/CalcTime";
+
 import * as THREE from "three";
-
-// Making a three js scene
-const scene:THREE.Scene = new THREE.Scene();
-const camera:THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-const renderer:THREE.WebGLRenderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.getElementById("webgl").appendChild(renderer.domElement);
 
 // all planets in one array
 
@@ -144,9 +139,37 @@ export var planets:Planet[] = [
         apoapsis: 0
     })
 ];
+
+
 // SOURCE: https://nssdc.gsfc.nasa.gov/planetary/factsheet/
 
+/*
 ConfigPrompt.then((config: any) => {
     // Do something when user got prompted about the config
     DrawPath(config);
 })
+*/
+
+
+// Making a three js scene
+var CamPos: CamOrigin = new CamOrigin()
+
+const scene:THREE.Scene = new THREE.Scene();
+const camera:THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer:THREE.WebGLRenderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById("webgl").appendChild(renderer.domElement);
+
+
+const animate = function () {
+    requestAnimationFrame( animate );
+
+    planets.forEach((planet: Planet) => {
+        planet.DrawPlanet()
+    });
+
+    renderer.render( scene, camera );
+};
+
+animate();
